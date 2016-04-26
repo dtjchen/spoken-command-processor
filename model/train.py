6ebaf172e3daf337862dc0a2d59b42165ccdc271
Miguel Amigot <miguel.amigot@gmail.com>
2016-04-26 13:57:01 -0400

    Predict using testing set

--- model/train.py
+++ model/train.py
@@ -22,30 +22,40 @@ def train():
     X_train, y_train = dataset.load_training_data()
     X_test, y_test = dataset.load_test_data()
     classes = dataset.load_unique_phonemes_as_class_numbers()
+    #print(max(classes.values()))
+    #print(classes)
+    #exit()
 
     input_dim = X_train.shape[1]
     output_dim = np.max(y_train) + 1
     hidden_num = 256
 
-    X_train = X_train[:20000, :]
-    y_train = y_train[:20000]
+    TRAINING_COUNT = 1000000
+    X_train = X_train[:TRAINING_COUNT, :]
+    y_train = y_train[:TRAINING_COUNT]
 
     y_train_out = dec2onehot(y_train, output_dim)
 
     model = Sequential()
 
-    model.add(Dense(input_dim=input_dim,output_dim=hidden_num))
+    model.add(Dense(input_dim=input_dim, output_dim=hidden_num))
     model.add(Activation('sigmoid'))
     model.add(Dense(output_dim=output_dim))
     model.add(Activation('softmax'))
 
     #optimizer = SGD(lr=0.01, momentum=0.9, nesterov=True)
-    optimizer= Adadelta()
-    loss='categorical_crossentropy'
+    optimizer = Adadelta()
+    loss ='categorical_crossentropy'
 
     model.compile(loss=loss, optimizer=optimizer)
     print model.summary()
 
-    hist = model.fit(X_train, y_train_out, shuffle=True, batch_size=256, nb_epoch=100, verbose=1)
-    P.plot(hist.history['loss'])
-    P.show()
+    hist = model.fit(X_train, y_train_out, shuffle=True, batch_size=256, nb_epoch=200, verbose=1)
+
+    out = model.predict_classes(X_test, batch_size=256, verbose=0)
+    #print(out[:50])
+    #print(max(out))
+    print(sum(out == y_test) * 1.0 / len(out))
+
+    #P.plot(hist.history['loss'])
+    #P.show()
