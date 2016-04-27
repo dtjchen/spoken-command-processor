d46d0c22bee7439454c947c36265f66f3c3244c7
Miguel Amigot <miguel.amigot@gmail.com>
2016-04-27 12:59:54 -0400

    Discard

--- model/train.py
+++ model/train.py
@@ -18,19 +18,45 @@ def dec2onehot(dec, output_dim):
     ret[range(0,num),dec]=1
     return ret
 
+def onehot_matrix(class_vec, number_of_classes):
+    """
+    >>> onehot_matrix(np.array([1, 0, 3]), number_of_classes=4)
+    [[ 0.  1.  0.  0.]
+     [ 1.  0.  0.  0.]
+     [ 0.  0.  0.  1.]]
+    """
+    onehot = np.zeros(shape=(class_vec.shape[0], number_of_classes))
+
+
+    pass
+
 def train():
+    """
+    lol = np.array([1, 0, 3])
+    output_dim = 4
+    print(dec2onehot(lol, output_dim))
+
+    exit()
+    """
+
+
     X_train, y_train = dataset.load_training_data()
     X_test, y_test = dataset.load_test_data()
     classes = dataset.load_unique_phonemes_as_class_numbers()
-    #print(max(classes.values()))
-    #print(classes)
+    print(max(classes.values()))
+    print(min(classes.values()))
+    print(len(classes))
     #exit()
 
     input_dim = X_train.shape[1]
     output_dim = np.max(y_train) + 1
+
+    print(output_dim)
+    exit()
+
     hidden_num = 256
 
-    TRAINING_COUNT = 1000000
+    TRAINING_COUNT = 10000
     X_train = X_train[:TRAINING_COUNT, :]
     y_train = y_train[:TRAINING_COUNT]
 
@@ -50,7 +76,7 @@ def train():
     model.compile(loss=loss, optimizer=optimizer)
     print model.summary()
 
-    hist = model.fit(X_train, y_train_out, shuffle=True, batch_size=256, nb_epoch=200, verbose=1)
+    hist = model.fit(X_train, y_train_out, shuffle=True, batch_size=256, nb_epoch=150, verbose=1)
 
     out = model.predict_classes(X_test, batch_size=256, verbose=0)
     #print(out[:50])
