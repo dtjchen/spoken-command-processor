18338a33f766ae3a22d7bb04cc35b7452bc90e47
Miguel Amigot <miguel.amigot@gmail.com>
2016-04-27 18:59:36 -0400

    Show how to train and test 'speech2phonemes'

--- driver.py
+++ driver.py
@@ -4,7 +4,13 @@ processor (just call functions from all modules)
 
 rough process:
 - Record_user_command(): # wait until the user says something (while silence:
-    don’t record) and, once the user has spoken for X seconds, record the signal
+    don't record) and, once the user has spoken for X seconds, record the signal
 - Interpret the user command
 - Let the processor know that that command was called
 """
+from model import speech2phonemes
+
+
+if __name__ == '__main__':
+    speech2phonemes.train(summarize=False, data_limit=10000)
+    speech2phonemes.test()
