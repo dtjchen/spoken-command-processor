e76760545707f77f8380ddf8d88159586ccbc1a5
Miguel Amigot <miguel.amigot@gmail.com>
2016-05-03 19:04:12 -0400

    Return additional arguments from wavfile_to_mfccs

--- model/utils.py
+++ model/utils.py
@@ -28,10 +28,10 @@ def wavfile_to_mfccs(wavfile):
     sampling_rate, frames = scipy.io.wavfile.read(wavfile)
 
     segment_duration_ms = 20
-    segment_duration_frames = int((segment_duration_ms / 1000.) * sampling_rate)
+    n_fft = int((segment_duration_ms / 1000.) * sampling_rate)
 
     hop_duration_ms = 10
-    hop_duration_frames = int((hop_duration_ms / 1000.) * sampling_rate)
+    hop_length = int((hop_duration_ms / 1000.) * sampling_rate)
 
     mfcc_count = 13
 
@@ -39,11 +39,11 @@ def wavfile_to_mfccs(wavfile):
         y=frames,
         sr=sampling_rate,
         n_mfcc=mfcc_count,
-        hop_length=hop_duration_frames,
-        n_fft=segment_duration_frames
+        hop_length=hop_length,
+        n_fft=n_fft
     )
     mfcc_delta = librosa.feature.delta(mfccs)
     mfcc_delta2 = librosa.feature.delta(mfccs, order=2)
     mfccs_and_deltas = np.vstack([mfccs, mfcc_delta, mfcc_delta2])
     
-    return mfccs_and_deltas
+    return mfccs_and_deltas, hop_length, n_fft
