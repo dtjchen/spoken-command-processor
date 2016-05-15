9c31c70648965e1ef24467efd67f18f6d50385d6
dtjchen <dtjchen@users.noreply.github.com>
2016-05-15 13:35:45 -0400

    Add import, complete simple regulator

--- model/regulator.py
+++ model/regulator.py
@@ -1,3 +1,4 @@
+from itertools import groupby
 
 def regulate(raw_phonemes, max_allowed):
     """
@@ -11,17 +12,21 @@ def regulate(raw_phonemes, max_allowed):
         list of max_allowed elements encapsulating the "correct" phonemes; if
         the list is not filled, right-pad it with zeros.
     """
+    # remove single values
+    seq = filter_sequence(raw_phonemes)
+    reg_phonemes = [k[0] for k in groupby(seq)]
     
-    
-    pass
+    # does not yet address if sequence is too long (truncate? filter with min_combo = 3?)
+    return pad_list(reg_phonemes, 0, max_allowed)
 
 def filter_sequence(seq, min_combo=2):
     # simple way
     combos = [[k, len(list(g))] for k, g in groupby(seq)]
+    # one line?: return [x for combo in combos for x in [combo[0]]*combo[1] if combo[1] >= min_combo]
     nseq = []
     for combo in combos:
         if combo[1] >= min_combo:
-            # preserve duplication for repeated filtering
+            # preserve duplication for repeated filtering 
             nseq.extend([combo[0]]*combo[1])
     return nseq
 
