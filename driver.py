6d14f3d35bc3c36802fd59a049bb0ba9fa529bbd
Miguel Amigot <miguel.amigot@gmail.com>
2016-05-17 01:06:14 -0400

    Suppress stderr (cleaner output)

--- driver.py
+++ driver.py
@@ -1,26 +1,28 @@
-"""
-allow user to record, make a prediction and call the necessary function in the
-processor (just call functions from all modules)
+import sys, os
 
-rough process:
-- Record_user_command(): # wait until the user says something (while silence:
-    don't record) and, once the user has spoken for X seconds, record the signal
-- Interpret the user command
-- Let the processor know that that command was called
-"""
 from model import speech2phonemes, phonemes2text
 from model import dataset
+import processor
+
+
+# Suppress stderr from the output
+null = open(os.devnull,'wb')
+sys.stderr = null
 
 
 def test_speech2phonemes():
-    speech2phonemes.train(summarize=False, data_limit=10000)
+    #speech2phonemes.train(summarize=True, data_limit=100000)
     speech2phonemes.test()
 
 def test_phonemes2text():
-    phonemes2text.train(summarize=True, data_limit=None)
+    #phonemes2text.train(summarize=False, data_limit=None)
     phonemes2text.test()
 
 
 if __name__ == '__main__':
     #test_speech2phonemes()
-    test_phonemes2text()
+    #test_phonemes2text()
+    #processor.register()
+
+    #processor.register()
+    processor.parse()
