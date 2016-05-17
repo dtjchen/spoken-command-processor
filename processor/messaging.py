42c6e4ee3a64e658111327fc225fee6e571a3da9
Miguel Amigot <miguel.amigot@gmail.com>
2016-05-17 13:46:30 -0400

    Socket communication

--- processor/messaging.py
+++ processor/messaging.py
@@ -1,66 +1,56 @@
 import os
+from contextlib import closing
 import socket
 import sys
 
-# Host for all processes
-HOST = 'localhost'
 
-# Port of this process
-PORT = os.environ['SPEECH_PORT']
-
-def send_message(message_str, destination_port):
+def send(message, port, host='localhost'):
     """Open a connection between PORT and destination_port, and send the
     message.
     """
     # Create a TCP/IP socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    destination_addr = (HOST, destination_port)
-    try: # Connect and send message
-        s.connect(destination_addr)
-        s.sendall(message_str)
-    except socket.error as err:
-        print('Error Code : %s, Message %s' % (str(err[0]), err[1]))
-    finally:
-        s.close()
-
-    # Send the message
-    print('sent message to %d: %s' % (destination_port, message_str))
+        destination_addr = (host, port)
+        try: # Connect and send message
+            s.connect(destination_addr)
+            s.sendall(message)
+        except socket.error as err:
+            print('Error Code : %s, Message %s' % (str(err[0]), err[1]))
 
     return True
 
-def listen():
+def listen(port, host='localhost'):
     """Listen on PORT for new connections on a continuous basis. Accept them
     and print their messages.
     """
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    try:
-        # Bind socket to address
-        s.bind((HOST, PORT))
+        try:
+            # Bind socket to address
+            s.bind((host, port))
 
-        # Start listening
-        backlog = 10
-        s.listen(backlog)
+            # Start listening
+            backlog = 10
+            s.listen(backlog)
 
-        # Block to accept connection
-        recv_bytes = 1024
-        while True:
-            conn, addr = s.accept()
-            message = conn.recv(recv_bytes)
+            # Block to accept connection
+            recv_bytes = 1024
+            while True:
+                conn, addr = s.accept()
+                message = conn.recv(recv_bytes)
 
-            # Print message
-            print('Message (%d:%d): %s' % (addr[0], addr[1], message))
+                # Print message
+                print('Message (%s:%s): %s' % (addr[0], addr[1], message))
 
-            conn.close()
+                conn.close()
 
-    except socket.error as err:
-        print('Error Code : %d, Message %s' % (err[0], err[1]))
-        sys.exit()
-    finally:
-        s.close()
+        except socket.error as err:
+            print('Error Code : %d, Message %s' % (err[0], err[1]))
+            sys.exit()
+
 
 if __name__ == '__main__':
     listen()
